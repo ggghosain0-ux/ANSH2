6137bfc34f92ca87030f471f5c16424aa9aeab9b
SKYDO YT <ggghosain0@gmail.com>
2026-06-17 15:39:42 +0530

    Create 24code.sh

---
+++ 24code.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+clear
+echo "=================================="
+echo "         24CODE RUNNER"
+echo "=================================="
+
+python3 <(curl -s https://raw.githubusercontent.com/ggghosain0-ux/Panel/main/24code)
